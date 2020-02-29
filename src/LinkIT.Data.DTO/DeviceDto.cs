d1ce41dd84ff4c8739320c40c5b9dbccef2c1aaf
Frederic Vets <frederic.vets@gmail.com>
2020-02-29 18:54:52 +0100

    Implementing IEquatable<T>

--- src/LinkIT.Data.DTO/DeviceDto.cs
+++ src/LinkIT.Data.DTO/DeviceDto.cs
@@ -13,35 +13,6 @@ namespace LinkIT.Data.DTO
 
 		public string Type { get; set; }
 
-		// Auto-generated equality methods.
-		public override bool Equals(object obj)
-		{
-			return Equals(obj as DeviceDto);
-		}
-
-		public bool Equals(DeviceDto other)
-		{
-			return other != null &&
-				   EqualityComparer<long?>.Default.Equals(Id, other.Id) &&
-				   Tag == other.Tag &&
-				   Owner == other.Owner &&
-				   Brand == other.Brand &&
-				   Type == other.Type;
-		}
-
-		public override int GetHashCode()
-		{
-			var hashCode = -2085744510;
-
-			hashCode = hashCode * -1521134295 + Id.GetHashCode();
-			hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Tag);
-			hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Owner);
-			hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Brand);
-			hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Type);
-
-			return hashCode;
-		}
-
 		public static bool operator ==(DeviceDto left, DeviceDto right)
 		{
 			return EqualityComparer<DeviceDto>.Default.Equals(left, right);
@@ -50,6 +21,33 @@ namespace LinkIT.Data.DTO
 		public static bool operator !=(DeviceDto left, DeviceDto right)
 		{
 			return !(left == right);
+		}
+
+		public override bool Equals(object obj)
+		{
+			return Equals(obj as DeviceDto);
+		}
+
+		public override int GetHashCode()
+		{
+			var hashCode = -1165835544;
+			hashCode = hashCode * -1521134295 + base.GetHashCode();
+			hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Tag);
+			hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Owner);
+			hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Brand);
+			hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Type);
+
+			return hashCode;
+		}
+
+		public bool Equals(DeviceDto other)
+		{
+			return other != null &&
+				   base.Equals(other) &&
+				   Tag == other.Tag &&
+				   Owner == other.Owner &&
+				   Brand == other.Brand &&
+				   Type == other.Type;
 		}
 	}
 }
--- src/LinkIT.Data.DTO/Dto.cs
+++ src/LinkIT.Data.DTO/Dto.cs
@@ -1,7 +1,36 @@
-﻿namespace LinkIT.Data.DTO
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinkIT.Data.DTO
 {
-	public abstract class Dto
+	public abstract class Dto : IEquatable<Dto>
 	{
 		public long? Id { get; set; }
+
+		public static bool operator ==(Dto left, Dto right)
+		{
+			return EqualityComparer<Dto>.Default.Equals(left, right);
+		}
+
+		public static bool operator !=(Dto left, Dto right)
+		{
+			return !(left == right);
+		}
+
+		public override bool Equals(object obj)
+		{
+			return Equals(obj as Dto);
+		}
+
+		public override int GetHashCode()
+		{
+			return 2108858624 + Id.GetHashCode();
+		}
+
+		public bool Equals(Dto other)
+		{
+			return other != null &&
+				   Id == other.Id;
+		}
 	}
 }
