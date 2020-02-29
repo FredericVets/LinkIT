183c0decff41bc9f825cd2dc9b8c9a89171b0ab8
Frederic Vets <frederic.vets@gmail.com>
2020-02-29 21:23:53 +0100

    Playing with Expression-bodied members and local functions.

--- src/LinkIT.Data/Paging/OrderBy.cs
+++ src/LinkIT.Data/Paging/OrderBy.cs
@@ -32,44 +32,6 @@ namespace LinkIT.Data.Paging
 			Order = order;
 		}
 
-		private static Order GetOrderFor(string input)
-		{
-			if (string.IsNullOrWhiteSpace(input))
-				throw new ArgumentNullException("input");
-
-			char first = input.First();
-
-			if (first == ASCENDING_CHARACTER)
-				return Order.ASCENDING;
-
-			if (first == DESCENDING_CHARACTER)
-				return Order.DESCENDING;
-
-			// The default.
-			return Order.ASCENDING;
-		}
-
-		private static char GetCharFor(Order input)
-		{
-			if (input == Order.ASCENDING)
-				return ASCENDING_CHARACTER;
-
-			if (input == Order.DESCENDING)
-				return DESCENDING_CHARACTER;
-
-			throw new InvalidOperationException($"Unknown Order enum value : {input}.");
-		}
-
-		private static bool StartsWithSortingChar(string input)
-		{
-			if (string.IsNullOrWhiteSpace(input))
-				throw new ArgumentNullException("input");
-
-			char first = input.First();
-
-			return new[] { ASCENDING_CHARACTER, DESCENDING_CHARACTER }.Contains(first);
-		}
-
 		public string Name { get; }
 
 		public Order Order { get; }
@@ -79,10 +41,7 @@ namespace LinkIT.Data.Paging
 			return EqualityComparer<OrderBy>.Default.Equals(left, right);
 		}
 
-		public static bool operator !=(OrderBy left, OrderBy right)
-		{
-			return !(left == right);
-		}
+		public static bool operator !=(OrderBy left, OrderBy right) => !(left == right);
 
 		public static OrderBy Parse(string input)
 		{
@@ -102,22 +61,38 @@ namespace LinkIT.Data.Paging
 			if (!Regex.IsMatch(input, REGEX_PATTERN))
 				return false;
 
-			if (StartsWithSortingChar(input))
+			char first = input.First();
+			if (IsSortingChar(first))
 			{
-				result = new OrderBy(input.Substring(1), GetOrderFor(input));
+				result = new OrderBy(input.Substring(1), GetOrderFor(first));
 
 				return true;
 			}
 
-			result = new OrderBy(input, GetOrderFor(input));
+			result = new OrderBy(input, GetOrderFor(first));
 
 			return true;
+
+			// Local functions.
+			bool IsSortingChar(char value)
+			{
+				return new[] { ASCENDING_CHARACTER, DESCENDING_CHARACTER }.Contains(value);
+			}
+
+			Order GetOrderFor(char value)
+			{
+				if (value == ASCENDING_CHARACTER)
+					return Order.ASCENDING;
+
+				if (value == DESCENDING_CHARACTER)
+					return Order.DESCENDING;
+
+				// The default.
+				return Order.ASCENDING;
+			}
 		}
 
-		public override bool Equals(object obj)
-		{
-			return Equals(obj as OrderBy);
-		}
+		public override bool Equals(object obj) => Equals(obj as OrderBy);
 
 		public override int GetHashCode()
 		{
@@ -133,6 +108,18 @@ namespace LinkIT.Data.Paging
 			char prefix = GetCharFor(Order);
 
 			return $"{prefix}{Name}";
+
+			// Local function.
+			char GetCharFor(Order input)
+			{
+				if (input == Order.ASCENDING)
+					return ASCENDING_CHARACTER;
+
+				if (input == Order.DESCENDING)
+					return DESCENDING_CHARACTER;
+
+				throw new InvalidOperationException($"Unknown Order enum value : {input}.");
+			}
 		}
 
 		public bool Equals(OrderBy other)
