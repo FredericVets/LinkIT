649afa8f6d92f8c8bd17963a6298a1f4c0aa96fb
Frederic Vets <frederic.vets@gmail.com>
2020-04-09 09:57:21 +0200

    Create one script that conatins all the ddl statements instead of several script.

--- database/sql/drop_and_create_all.sql
+++ database/sql/drop_and_create_all.sql
@@ -4,7 +4,73 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
+DROP TABLE IF EXISTS [dbo].[SpecialOwner];
 DROP TABLE IF EXISTS [dbo].[AssetHistory];
+DROP TABLE IF EXISTS [dbo].[Asset];
+DROP TABLE IF EXISTS [dbo].[Product];
+DROP TABLE IF EXISTS [dbo].[UserRole];
+
+GO
+
+
+
+CREATE TABLE [dbo].[SpecialOwner](
+	[Id] bigint PRIMARY KEY IDENTITY(1,1) NOT NULL,
+	[CreationDate] DateTime2 NOT NULL,
+	[CreatedBy] varchar(30) NOT NULL,
+	[ModificationDate] DateTime2 NOT NULL,
+	[ModifiedBy] varchar(30) NOT NULL,
+	[Name] varchar(30) NOT NULL,
+	[Remark] varchar(30)
+);
+
+GO
+
+
+
+CREATE TABLE [dbo].[Product](
+	[Id] bigint PRIMARY KEY IDENTITY(1,1) NOT NULL,
+	[CreationDate] DateTime2 NOT NULL,
+	[CreatedBy] varchar(30) NOT NULL,
+	[ModificationDate] DateTime2 NOT NULL,
+	[ModifiedBy] varchar(30) NOT NULL,
+	[Brand] varchar(30) NOT NULL,
+	[Type] varchar(30) NOT NULL
+);
+
+GO
+
+
+
+CREATE TABLE [dbo].[Asset](
+	[Id] bigint PRIMARY KEY IDENTITY(1,1) NOT NULL,
+	[CreationDate] DateTime2 NOT NULL,
+	[CreatedBy] varchar(30) NOT NULL,
+	[ModificationDate] DateTime2 NOT NULL,
+	[ModifiedBy] varchar(30) NOT NULL,
+	[IctsReference] varchar(30),
+	[Tag] varchar(15) NOT NULL,
+		CONSTRAINT UQ_Asset_Tag UNIQUE (Tag),
+	[Serial] varchar(30),
+	[ProductId] bigint NOT NULL,
+		CONSTRAINT FK_Asset_Product_Id FOREIGN KEY (ProductId)
+		REFERENCES [dbo].[Product](Id),
+	[Description] varchar(500),
+	[InvoiceDate] DateTime2,
+	[InvoiceNumber] varchar(30),
+	[Price] decimal(18,2),
+	[PaidBy] varchar(30) NOT NULL,
+	[Owner] varchar(30) NOT NULL,
+	[InstallDate] DateTime2,
+	[InstalledBy] varchar(30),
+	[Remark] varchar(500),
+	[TeamAsset] bit NOT NULL,
+	[Deleted] bit NOT NULL
+);
+
+GO
+
+
 
 CREATE TABLE [dbo].[AssetHistory](
 	[Id] bigint PRIMARY KEY IDENTITY(1,1) NOT NULL,
@@ -35,6 +101,8 @@ CREATE TABLE [dbo].[AssetHistory](
 );
 
 GO
+
+
 
 DROP INDEX IF EXISTS IX_AssetHistory_AssetId ON [dbo].[AssetHistory];
 CREATE NONCLUSTERED INDEX IX_AssetHistory_AssetId ON [dbo].[AssetHistory](AssetId);
@@ -71,5 +139,16 @@ AS
 		ins.InvoiceDate, ins.InvoiceNumber, ins.Price, ins.PaidBy, ins.Owner, ins.InstallDate, ins.InstalledBy, ins.Remark, ins.TeamAsset, ins.Deleted 
 	FROM INSERTED ins
 ;
+
+GO
+
+
+
+CREATE TABLE [dbo].[UserRole](
+	[Id] bigint PRIMARY KEY IDENTITY(1,1) NOT NULL,
+	[UserName] varchar(15) NOT NULL,
+		CONSTRAINT UQ_UserRole_UserName UNIQUE (UserName),
+	[Roles] varchar(256) NOT NULL
+);
 
 GO
