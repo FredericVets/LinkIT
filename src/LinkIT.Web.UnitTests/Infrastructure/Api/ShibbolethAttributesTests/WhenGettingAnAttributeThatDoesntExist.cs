531d5061d300b5f77334889b8c09f3e07786f6a5
Frederic Vets <frederic.vets@gmail.com>
2020-04-10 10:02:48 +0200

    Added support to get attributes from the Request.ServerVariables.

---
+++ src/LinkIT.Web.UnitTests/Infrastructure/Api/ShibbolethAttributesTests/WhenGettingAnAttributeThatDoesntExist.cs
@@ -0,0 +1,36 @@
+﻿using LinkIT.Web.Infrastructure.Api;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Specialized;
+
+namespace LinkIT.Web.UnitTests.Infrastructure.Api.ShibbolethAttributesTests
+{
+	[TestClass]
+	public class WhenGettingAnAttributeThatDoesntExist
+	{
+		private ShibbolethAttributes _sut;
+
+		private NameValueCollection CreateValues() =>
+			new NameValueCollection()
+			{
+				{ "whatever", "whatever" },
+			};
+
+		[TestInitialize]
+		public void Setup()
+		{
+			var mock = MockFactory.Create(CreateValues);
+
+			_sut = new ShibbolethAttributes(mock.Object);
+		}
+
+		[TestMethod]
+		public void ThenTheAttributeIsNotFound()
+		{
+			bool result = _sut.TryGet("iDontExist", out _);
+			Assert.IsFalse(result);
+
+			Assert.ThrowsException<ArgumentException>(() => _sut.Get("iDontExist"));
+		}
+	}
+}
